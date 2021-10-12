1abaca9f376a96527c6c5a505095bed8261e9810
sakibssamad <sssakib.info@gmail.com>
2021-10-12 17:38:35 +0600

    Custom list added successfully.

--- lib/user_list.dart
+++ lib/user_list.dart
@@ -33,79 +33,146 @@ class _UserListState extends State<UserList> {
                     builder: (_) => DetailOfUser(
                         userLists[index]
                     ),),);},
-              child: new Card(
-                elevation: 5,
-                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                child: Container(
-                  padding: EdgeInsets.all(8),
-
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Name: ${userLists[index].name.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Mobile: ${userLists[index].mobile.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Email: ${userLists[index].email.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Age: ${userLists[index].age.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Gender: ${userLists[index].gender.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Address: ${userLists[index].address.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8, right: 8),
-                          child: Text(
-                            "Role: ${userLists[index].roledesc.toString()}",
-                            style: TextStyle(
-                                fontSize: 16, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-
-
-                      ]),
-
-                ),
-
-              ),
+               child: Container(
+                 width: double.infinity,
+                 height: 250,
+                 child: Card(
+                   shadowColor: Colors.indigo,
+                   shape: RoundedRectangleBorder(
+                     borderRadius: BorderRadius.circular(15.0),
+                   ),
+                   color: Colors.indigo.withOpacity(1),
+                   child: Card(
+                     shadowColor: Colors.indigo,
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(15.0),
+                     ),
+                     color: Colors.white70.withOpacity(1),
+                     
+                     child: Padding(
+                       padding: EdgeInsets.all(10),
+                       child: Column(
+                         crossAxisAlignment: CrossAxisAlignment.center,
+                         children: [
+                           Expanded(
+                             flex: 1,
+                             child: Row(
+                               children: [
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     
+                                     children: [
+                                       Text("Name"),
+                                       Text(
+                                         "${userLists[index].name.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     children: [
+                                       Text("Mobile"),
+                                       Text(
+                                         "${userLists[index].mobile.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                           Expanded(
+                             flex: 1,
+                             child: Row(
+                               children: [
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     children: [
+                                       Text("Email"),
+                                       Text(
+                                         "${userLists[index].email.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     children: [
+                                       Text("Age"),
+                                       Text(
+                                         "${userLists[index].age.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                           Expanded(
+                             flex: 1,
+                             child: Row(
+                               children: [
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     children: [
+                                       Text("Gender"),
+                                       Text(
+                                         "${userLists[index].gender.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                                 Expanded(
+                                   flex: 1,
+                                   child: Column(
+                                     children: [
+                                       Text("Address"),
+                                       Text(
+                                         "${userLists[index].address.toString()}",
+                                         style: TextStyle(fontWeight: FontWeight.bold),
+                                       )
+                                     ],
+                                   ),
+                                 ),
+                               ],
+                             ),
+                           ),
+                           Expanded(
+                             flex: 1,
+                             child: Row(
+                               mainAxisAlignment: MainAxisAlignment.center,
+                               children: [
+                                 Expanded(
+                                     flex: 1,
+                                     child: Column(
+                                       children: [
+                                         Text("Role"),
+                                         Text(
+                                           "${userLists[index].roledesc.toString()}",
+                                           style: TextStyle(fontWeight: FontWeight.bold),
+                                         )
+                                       ],
+                                     )),
+                               ],
+                             ),
+                           ),
+                         ],
+                       ),
+                     ),
+                   ),
+                 ),
+               ),
           );
 
         },
